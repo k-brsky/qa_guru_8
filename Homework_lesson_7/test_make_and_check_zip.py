b7524190e67e64ec143c4563507ed2d6e3d71e7d
Kristina Borisovskaya <krissstinkaaa163@gmail.com>
2023-10-17 10:07:49 +0400

    Добавила тест для текстового файла и xlsx

--- Homework_lesson_7/test_make_and_check_zip.py
+++ Homework_lesson_7/test_make_and_check_zip.py
@@ -1,7 +1,6 @@
 from zipfile import ZipFile, ZIP_DEFLATED
 import os
 from os.path import basename
-import glob
 from openpyxl import load_workbook
 
 def test_make_zip():
@@ -9,27 +8,38 @@ def test_make_zip():
     file_dir = os.listdir(path)
     os.mkdir('tmp')
     with ZipFile('tmp/homework.zip', mode='w', compression=ZIP_DEFLATED) as zf:
-        # тут в архиве создается папка resources, в ней нужные файлы
         for file in file_dir:
             add_file = os.path.join(path, file)
             zf.write(add_file, basename(add_file))
 
-            # тут тоже сначала resources
-        for root, dirs, files in os.walk(path):
-            for file in files:
-                zf.write(os.path.join(root, file))
+def test_check_txt():
+    with open('resources/file_txt.txt') as f:
+        text_resources = f.read()
 
-        # тут сначала создает папку homework(мб по названию архива), а уже в ней нужные файлы
-        zf.write('resources/file_pdf.pdf', basename('resources/file_pdf.pdf'))
-        zf.write('resources/file_txt.txt', basename('resources/file_txt.txt'))
-        zf.write('resources/test_sheet_xls.xls', basename('resources/test_sheet_xls.xls'))
-        zf.write('resources/test_sheet_xlsx.xlsx', basename('resources/test_sheet_xlsx.xlsx'))
-
-def test_check_zip():
     with ZipFile('tmp/homework.zip') as zf:
-        text = zf.read('file_txt.txt')
-        assert "текстовый файлик" in text
+        text_zip = zf.read('file_txt.txt').decode()
+        assert text_resources == text_zip
 
-        wb = load_workbook('test_sheet_xlsx.xlsx')
-        sheets = wb.sheetnames
+def test_check_xlsx():
+    with open('resources/test_sheet_xlsx.xlsx') as f:
+        wb_resources = load_workbook('resources/test_sheet_xlsx.xlsx')
+        sheet_resources = wb_resources.active
+        cell_name_resources = sheet_resources.cell(row = 3, column = 1)
+        cell_age_resources = sheet_resources.cell(row = 3, column = 2)
+
+    with ZipFile('tmp/homework.zip') as zf:
+        wb_zip = load_workbook(zf.open('test_sheet_xlsx.xlsx'))
+        sheet_zip = wb_zip.active
+        cell_name_zip = sheet_zip.cell(row = 3, column = 1)
+        cell_age_zip = sheet_zip.cell(row = 3, column = 2)
+        assert cell_name_resources == cell_name_zip
+        assert cell_age_resources == cell_age_zip
+
+
+
+
+
+
+
+
 
