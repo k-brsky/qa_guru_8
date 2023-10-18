1843f10d60fbb68d8eae5a96382004b1c0bb0043
Kristina Borisovskaya <krissstinkaaa163@gmail.com>
2023-10-18 10:56:32 +0400

    Добавила тест для текстового файла и xls

--- Homework_lesson_7/test_make_and_check_zip.py
+++ Homework_lesson_7/test_make_and_check_zip.py
@@ -2,6 +2,7 @@ from zipfile import ZipFile, ZIP_DEFLATED
 import os
 from os.path import basename
 from openpyxl import load_workbook
+from xlrd import open_workbook
 
 def test_make_zip():
     path = 'resources'
@@ -24,16 +25,28 @@ def test_check_xlsx():
     with open('resources/test_sheet_xlsx.xlsx') as f:
         wb_resources = load_workbook('resources/test_sheet_xlsx.xlsx')
         sheet_resources = wb_resources.active
-        cell_name_resources = sheet_resources.cell(row = 3, column = 1)
-        cell_age_resources = sheet_resources.cell(row = 3, column = 2)
+        cell_name_resources = sheet_resources.cell(row = 3, column = 1).value
+        cell_age_resources = sheet_resources.cell(row = 3, column = 2).value
 
     with ZipFile('tmp/homework.zip') as zf:
         wb_zip = load_workbook(zf.open('test_sheet_xlsx.xlsx'))
         sheet_zip = wb_zip.active
-        cell_name_zip = sheet_zip.cell(row = 3, column = 1)
-        cell_age_zip = sheet_zip.cell(row = 3, column = 2)
+        cell_name_zip = sheet_zip.cell(row = 3, column = 1).value
+        cell_age_zip = sheet_zip.cell(row = 3, column = 2).value
         assert cell_name_resources == cell_name_zip
         assert cell_age_resources == cell_age_zip
+
+def test_check_xls():
+    work_book_resources = open_workbook('resources/test_sheet_xls.xls')
+    cell_animal_resorces = work_book_resources.sheet_by_index(0).cell_value(3, 0)
+    cell_count_legs_resources = work_book_resources.sheet_by_index(0).cell_value(3, 1)
+
+    with ZipFile('tmp/homework.zip') as zf:
+        work_book_zip = open_workbook(zf.open('test_sheet_xls.xls'))
+        cell_animal_zip = work_book_zip.sheet_by_index(0).cell_value(3, 0)
+        cell_count_legs_zip = work_book_zip.sheet_by_index(0).cell_value(3, 1)
+        assert cell_animal_resorces == cell_animal_zip
+        assert cell_count_legs_resources == cell_count_legs_zip
 
 
 
